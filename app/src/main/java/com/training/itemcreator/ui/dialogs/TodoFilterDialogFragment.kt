71902779cc063858f7106cfd83d28bdd96dddf8b
Giovanni Barbaro <giovanni.barbaro@accenture.com>
2020-05-28 17:26:27 +0200

    Added correct handling of temporary filter selections

--- app/src/main/java/com/training/itemcreator/ui/dialogs/TodoFilterDialogFragment.kt
+++ app/src/main/java/com/training/itemcreator/ui/dialogs/TodoFilterDialogFragment.kt
@@ -1,6 +1,7 @@
 package com.training.itemcreator.ui.dialogs
 
 import android.app.Dialog
+import android.content.DialogInterface
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CompoundButton
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.DialogFragment
@@ -21,29 +23,63 @@ class TodoFilterDialogFragment() : DialogFragment() {
 
     private lateinit var todoListViewModel: TodoListViewModel
 
+    private var lowPriorityButton: SwitchCompat? = null
+    private var midPriorityButton: SwitchCompat? = null
+    private var highPriorityButton: SwitchCompat? = null
+
+    private var updateFiltersButton: Button? = null
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext()).apply {
             setView(R.layout.dialog_filter_todo)
         }.create().apply {
-            this.setOnShowListener { _ ->
-                val lowPriorityButton = this.findViewById<SwitchCompat>(R.id.filter_low_switch)
-                val midPriorityButton = this.findViewById<SwitchCompat>(R.id.filter_mid_switch)
-                val highPriorityButton = this.findViewById<SwitchCompat>(R.id.filter_high_switch)
+            setOnShowListener { _ ->
+                lowPriorityButton = this.findViewById<SwitchCompat>(R.id.filter_low_switch)
+                midPriorityButton = this.findViewById<SwitchCompat>(R.id.filter_mid_switch)
+                highPriorityButton = this.findViewById<SwitchCompat>(R.id.filter_high_switch)
 
-                val updateFiltersButton = this.findViewById<Button>(R.id.update_filters_button)
+                updateFiltersButton = this.findViewById<Button>(R.id.update_filters_button)
 
-                lowPriorityButton?.isChecked = Priority.LOW in todoListViewModel.filterOptions
-                midPriorityButton?.isChecked = Priority.MEDIUM in todoListViewModel.filterOptions
-                highPriorityButton?.isChecked = Priority.HIGH in todoListViewModel.filterOptions
+                setupRadioButtons()
 
                 updateFiltersButton?.setOnClickListener { _ ->
-                    todoListViewModel.filterOptions = mutableListOf<Priority>().apply {
-                        if(lowPriorityButton?.isChecked == true) add(Priority.LOW)
-                        if(midPriorityButton?.isChecked == true) add(Priority.MEDIUM)
-                        if(highPriorityButton?.isChecked == true) add(Priority.HIGH)
+                    todoListViewModel.tempFilterOptions?.let {
+                        todoListViewModel.filterOptions = it
                     }
+                    todoListViewModel.tempFilterOptions = null
                     this.dismiss()
                 }
+            }
+        }
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        todoListViewModel.tempFilterOptions = null
+        super.onCancel(dialog)
+    }
+
+    fun setupRadioButtons() {
+        todoListViewModel.tempFilterOptions?.let {
+            lowPriorityButton?.isChecked = Priority.LOW in it
+            midPriorityButton?.isChecked = Priority.MEDIUM in it
+            highPriorityButton?.isChecked = Priority.HIGH in it
+        } ?: run {
+            lowPriorityButton?.isChecked = Priority.LOW in todoListViewModel.filterOptions
+            midPriorityButton?.isChecked = Priority.MEDIUM in todoListViewModel.filterOptions
+            highPriorityButton?.isChecked = Priority.HIGH in todoListViewModel.filterOptions
+
+            todoListViewModel.tempFilterOptions = todoListViewModel.filterOptions.toMutableList()
+        }
+
+        lowPriorityButton?.setOnCheckedChangeListener(onRadioChange(Priority.LOW))
+        midPriorityButton?.setOnCheckedChangeListener(onRadioChange(Priority.MEDIUM))
+        highPriorityButton?.setOnCheckedChangeListener(onRadioChange(Priority.HIGH))
+    }
+
+    private val onRadioChange = { priority: Priority ->
+        CompoundButton.OnCheckedChangeListener { _, isChecked ->
+            todoListViewModel.tempFilterOptions?.apply {
+                if (isChecked) add(priority) else remove(priority)
             }
         }
     }
--- app/src/main/java/com/training/itemcreator/viewmodel/TodoListViewModel.kt
+++ app/src/main/java/com/training/itemcreator/viewmodel/TodoListViewModel.kt
@@ -10,23 +10,30 @@ import kotlinx.coroutines.launch
 
 class TodoListViewModel(private val repository: TodoRepository) : ViewModel() {
 
+    // Sort option selected from SORT menu
     var sortOption = TodoSort.PRIORITY
         set(value) {
             field = value
             refreshData()
         }
+
+    // Filter options selected from FILTER menu
     var filterOptions = listOf(Priority.LOW, Priority.MEDIUM, Priority.HIGH)
         set(value) {
             field = value
             refreshData()
         }
+    // Filter to use when recreating filter dialog in Destruction -> Create
+    var tempFilterOptions: MutableList<Priority>? = null
 
+    // LiveData source used by the view, it changes sources once the filter or the sortoptions change
     val todoList = MediatorLiveData<List<Todo>>().apply {
         addSource(repository.fetchTodos(sortOption, filterOptions)) {
             this.value = it
         }
     }
 
+    // Flags used to notify when an operation is completed
     val todoAdded = MutableLiveData<Boolean>()
     val todoDeleted = MutableLiveData<Boolean>()
 
